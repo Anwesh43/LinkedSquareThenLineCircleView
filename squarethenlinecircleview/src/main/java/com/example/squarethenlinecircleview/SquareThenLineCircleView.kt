1e1f936e443bea09e0dbf2210fcfc546b6c40a32
Anwesh43 <anweshthecool0@gmail.com>
2021-04-26 00:59:48 +0530

    adding Canvas extension methods

--- squarethenlinecircleview/src/main/java/com/example/squarethenlinecircleview/SquareThenLineCircleView.kt
+++ squarethenlinecircleview/src/main/java/com/example/squarethenlinecircleview/SquareThenLineCircleView.kt
@@ -28,3 +28,32 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawSquareThenLineCircle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawRect(RectF(-size / 2, -(size / 2) * sf1, size / 2, -(size / 2) * sf2), paint)
+        drawLine(0f, (-size / 2) * sf2, 0f, (size / 2) * sf3, paint)
+        restore()
+    }
+    drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 0f, 360f * sf4, true, paint)
+    restore()
+}
+
+fun Canvas.drawSTLCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSquareThenLineCircle(scale, w, h, paint)
+}
